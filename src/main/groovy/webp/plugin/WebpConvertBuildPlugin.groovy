67cfbdc93f354a3248c0c74a6382e296849c6201
zhoujinhuang@163.com <zhoujinhuang@163.com>
2016-08-10 09:57:59 +0800

    add window support   ; add  enableSeparateBuildPerCPUArchitecture support

--- src/main/groovy/webp/plugin/WebpConvertBuildPlugin.groovy
+++ src/main/groovy/webp/plugin/WebpConvertBuildPlugin.groovy
@@ -3,6 +3,7 @@ package webp.plugin
 import com.android.build.gradle.AppPlugin
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import java.util.regex.*
 
 class WebpConvertBuildPlugin implements Plugin<Project> {
     WebpInfo config;
@@ -14,69 +15,100 @@ class WebpConvertBuildPlugin implements Plugin<Project> {
         def variants = hasApp ? project.android.applicationVariants : project.android.libraryVariants
 
         config = project.extensions.create("webpinfo", WebpInfo);
-
+        println "variants:" + variants
         project.afterEvaluate {
 
             variants.all { variant ->
-
+                println "variant:" + variant
+                println variant.getVariantData().getVariantConfiguration()
                 def flavor = variant.getVariantData().getVariantConfiguration().getFlavorName()
                 def buildType = variant.getVariantData().getVariantConfiguration().getBuildType().name
-
                 if (config.skipDebug == true && "${buildType}".contains("debug")) {
                     printlog "skipDebug webpConvertPlugin Task!!!!!!"
-
                     return
                 }
-
-                def dx = project.tasks.findByName("process${variant.name.capitalize()}Resources")
-                def webpConvertPlugin = "webpConvertPlugin${variant.name.capitalize()}"
-                project.task(webpConvertPlugin) << {
-                    String resPath = "${project.buildDir}/intermediates/res/${flavor}/${buildType}"
-                    println "resPath:" + resPath
-                    def dir = new File("${resPath}")
-                    dir.eachDirMatch(~/drawable[a-z0-9-]*/) { drawDir ->
-                        printlog "drawableDir:" + drawDir
-                        def file = new File("${drawDir}")
-                        file.eachFile { filename ->
-                            def name = filename.name
-                            def f = new File("${project.projectDir}/webp_white_list.txt")
-                            if (!f.exists()) {
-                                f.createNewFile()
-                            }
-                            def isInWhiteList = false
-                            f.eachLine { whiteName ->
-                                if (name.equals(whiteName)) {
-                                    isInWhiteList = true
-                                }
-                            }
-                            if (!isInWhiteList) {
-                                if (name.endsWith(".jpg") || name.endsWith(".png")) {
-                                    if (!name.contains(".9")) {
-
-                                        def picName = name.split('\\.')[0]
-                                        def suffix = name.split('\\.')[1]
-                                        printlog "find target pic >>>>>>>>>>>>>" + name
-                                        printlog "picName:" + picName
-
-
-                                        "cwebp -q 75 -m 6 ${filename} -o ${drawDir}/${picName}.webp".execute()
-                                        sleep(10)
-                                        "rm ${filename}".execute()
-                                        printlog "delete:" + "${filename}"
-                                        printlog "generate:" + "${drawDir}/${picName}.webp"
-
+                println project.tasks.getNames()
+                project.tasks.getNames().each { taskname ->
+                    Pattern pattern = Pattern.compile("process([-0-9a-zA-Z]+)${variant.name.capitalize()}Resources");
+                    Matcher m = pattern.matcher(taskname);
+                    while (m.find()) {
+                        def cpu = m.group(1);
+                        println "cpu: ${cpu}"
+                        println "process${cpu}${variant.name.capitalize()}Resources"
+                        def dx = project.tasks.findByName("process${cpu}${variant.name.capitalize()}Resources")
+                        println dx
+                        def webpConvertPlugin = "webpConvertPlugin${cpu}${variant.name.capitalize()}"
+                        project.task(webpConvertPlugin) << {
+                            String resPath = "${project.buildDir}/intermediates/${flavor}/res/merged/${buildType}"
+                            println "resPath:" + resPath
+                            def dir = new File("${resPath}")
+                            dir.eachDirMatch(~/drawable[a-z0-9-]*/) { drawDir ->
+                                println "drawableDir:" + drawDir
+                                def file = new File("${drawDir}")
+                                file.eachFile { filename ->
+                                    def name = filename.name
+                                    def f = new File("${project.projectDir}/webp_white_list.txt")
+                                    if (!f.exists()) {
+                                        f.createNewFile()
                                     }
+                                    def isInWhiteList = false
+                                    f.eachLine { whiteName ->
+                                        if (name.equals(whiteName)) {
+                                            isInWhiteList = true
+                                        }
+                                    }
+                                    if (!isInWhiteList) {
+                                        if (name.endsWith(".jpg") || name.endsWith(".png")) {
+                                            if (!name.contains(".9")) {
+
+                                                def picName = name.split('\\.')[0]
+                                                def suffix = name.split('\\.')[1]
+                                                printlog "find target pic >>>>>>>>>>>>>" + name
+                                                printlog "picName:" + picName
+
+                                                def cmd = "cwebp -q 75 -m 6 ${filename} -o ${drawDir}/${picName}.webp"
+                                                def p = cmd.execute()
+                                                try {
+                                                    def lines = p.getInputStream().readLines()
+                                                    lines.each { line ->
+                                                        printlog line
+                                                    }
+                                                    lines = p.getErrorStream().readLines()
+                                                    lines.each { line ->
+                                                        printlog line
+                                                    }
+                                                } catch (Exception e) {
+                                                    println "del ${cmd} failed"
+                                                    e.printStackTrace()
+                                                }
+
+                                                try {
+                                                    //printlog filename
+                                                    def del_res = filename.delete();
+                                                    printlog "${del_res} del ${filename}"
+                                                } catch (Exception e) {
+                                                    println "cmd  ${filename} failed"
+                                                    e.printStackTrace()
+                                                }
+                                                println "del ${filename}"
+                                                //"del ${filename}".execute()
+                                                printlog "delete:" + "${filename}"
+                                                printlog "generate:" + "${drawDir}/${picName}.webp"
+
+                                            }
+                                        }
+                                    }
+
                                 }
                             }
-
                         }
+
+                        project.tasks.findByName(webpConvertPlugin).dependsOn dx.taskDependencies.getDependencies(dx)
+                        dx.dependsOn project.tasks.findByName(webpConvertPlugin)
                     }
                 }
 
-                project.tasks.findByName(webpConvertPlugin).dependsOn dx.taskDependencies.getDependencies(dx)
-                dx.dependsOn project.tasks.findByName(webpConvertPlugin)
             }
-
         }
 
     }
